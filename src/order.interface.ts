1d68199fe75c41c22f0f4404b457ab3127ffad7a
NHKhanq <nhkhang061203@gmail.com>
2024-11-19 20:53:18 +0700

    publish

--- src/order.interface.ts
+++ src/order.interface.ts
@@ -9,7 +9,7 @@ export interface IOffer {
   newDeliveryDate: string;
   accepted: boolean;
   cancelled: boolean;
-  reason?: string; // this is the reason for extending the delivery date
+  reason?: string; 
 }
 
 export interface IExtendedDelivery {
