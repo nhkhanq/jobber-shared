3796298a17a3f040bf5444b142997f1aabd0117d
NHKhanq <nhkhang061203@gmail.com>
2024-11-19 14:59:50 +0700

    Setup publish workflow

---
+++ src/gateway-middleware.ts
@@ -0,0 +1,57 @@
+import JWT from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import { NotAuthorizedError } from './error-handler';
+
+const tokens: string[] = [
+  'auth',
+  'seller',
+  'gig',
+  'search',
+  'buyer',
+  'message',
+  'order',
+  'review',
+];
+
+export function verifyGatewayRequest(
+  req: Request,
+  _res: Response,
+  next: NextFunction
+): void {
+  if (!req.headers?.gatewaytoken) {
+    throw new NotAuthorizedError(
+      'Invalid request',
+      'verifyGatewayRequest() method: Request not coming from api gateway'
+    );
+  }
+
+  const token: string = req.headers?.gatewaytoken as string;
+
+  if (!token) {
+    throw new NotAuthorizedError(
+      'Invalid request',
+      'verifyGatewayRequest() method: Request not coming from api gateway'
+    );
+  }
+
+  try {
+    const payload: { id: string; iat: number } = JWT.verify(
+      token,
+      'd41d8cd98f00b204e9800998ecf8427e'
+    ) as { id: string; iat: number };
+
+    if (!tokens.includes(payload.id)) {
+      throw new NotAuthorizedError(
+        'Invalid request',
+        'verifyGatewayRequest() method: Request payload is invalid'
+      );
+    }
+  } catch (error) {
+    throw new NotAuthorizedError(
+      'Invalid request',
+      'verifyGatewayRequest() method: Request not coming from api gateway'
+    );
+  }
+
+  next();
+}
